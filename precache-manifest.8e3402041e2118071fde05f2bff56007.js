7df2b4db3146306184be545aad6944d5f6d5ec2d
rpounceyjr <rpounceyjr@gmail.com>
2020-08-21 05:50:04 -0400

    Updates

---
+++ precache-manifest.8e3402041e2118071fde05f2bff56007.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67de1aa6ba694de0a4e27c11a017867",
+    "url": "/hacker-news-search/index.html"
+  },
+  {
+    "revision": "0ed3a77e057ac0454b06",
+    "url": "/hacker-news-search/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1670e6bc6076ee0bb647",
+    "url": "/hacker-news-search/static/css/main.2ff120c3.chunk.css"
+  },
+  {
+    "revision": "0ed3a77e057ac0454b06",
+    "url": "/hacker-news-search/static/js/2.7610ae4b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/hacker-news-search/static/js/2.7610ae4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1670e6bc6076ee0bb647",
+    "url": "/hacker-news-search/static/js/main.b5e97b51.chunk.js"
+  },
+  {
+    "revision": "e4e6cd1ad5ec44b8ea42",
+    "url": "/hacker-news-search/static/js/runtime-main.6574640e.js"
+  }
+]);
